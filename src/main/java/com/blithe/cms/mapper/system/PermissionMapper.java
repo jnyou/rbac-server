6a63b589f2db7835647e79fc3ebeca0c9feca3da
Blithe0 <1447177020@qq.com>
2020-03-27 14:01:56 +0800

    # 删除菜单时删除对应的权限

--- src/main/java/com/blithe/cms/mapper/system/PermissionMapper.java
+++ src/main/java/com/blithe/cms/mapper/system/PermissionMapper.java
@@ -3,6 +3,9 @@ package com.blithe.cms.mapper.system;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.blithe.cms.pojo.system.Permission;
+import org.apache.ibatis.annotations.Param;
+
+import java.io.Serializable;
 
 /**
  * @Author: youjiannan
@@ -13,4 +16,10 @@ import com.blithe.cms.pojo.system.Permission;
  **/
 public interface PermissionMapper extends BaseMapper<Permission> {
 
+    /**
+     * 删除了菜单，权限就没了，所以应该也删除对应的角色id
+     * @param id
+     * @return
+     */
+    void deleteRolemissionByPid(@Param("pid") Serializable pid);
 }
--- src/main/java/com/blithe/cms/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/blithe/cms/service/impl/PermissionServiceImpl.java
@@ -4,7 +4,10 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.blithe.cms.mapper.system.PermissionMapper;
 import com.blithe.cms.pojo.system.Permission;
 import com.blithe.cms.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 /**
  * @Author: youjiannan
@@ -16,4 +19,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permission> implements PermissionService {
 
+    @Autowired
+    private PermissionMapper permissionMapper;
+
+    /**
+     * 删除了菜单，权限就没了，所以应该也删除对应的角色id
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean deleteById(Serializable id) {
+//        PermissionMapper baseMapper = this.baseMapper;
+        // 删除与角色关联的数据
+        permissionMapper.deleteRolemissionByPid(id);
+        return super.deleteById(id); // 删除权限表的数据
+    }
 }
